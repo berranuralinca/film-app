12308ca32fb1db96a2bc088aa959377202d2fe5e
berranuralinca <berranuralinca1108@gmail.com>
2026-08-28 20:22:09 +0300

    Add initial models and migrations for Movie, Genre, and Comment

---
+++ database/migrations/2026_08_28_171138_create_movies_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+
+    public function up(): void
+    {
+        Schema::create('movies', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId("genre_id")->constrained()->onDelete("cascade");
+            $table->string("title");
+            $table->text("description");
+            $table->string("director");
+            $table->integer("release_year");
+            $table->decimal("rating",3,1)->default(0);
+            $table->string("poster_image")->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('movies');
+    }
+};
---
+++ database/migrations/2026_08_28_171150_create_comments_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('movie_id')->constrained()->cascadeOnDelete();
+            $table->string('author_name');
+            $table->text('content');
+            $table->integer('rating')->default(5);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('comments');
+    }
+};
